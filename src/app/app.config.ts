49182d9df35c8693ce53e5afbd6b01e06b0e8eaf
Nicola Sereno <nicola.sereno@gmail.com>
2026-02-01 20:37:35 +0100

    fix: restore service worker configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import {provideHttpClient} from '@angular/common/http';
 import {VehiclesEffects} from './effects/vehicles.effects';
 import {MapsEffects} from './effects/maps.effects';
 import {BusesEffects} from './effects/buses.effects';
+import {provideServiceWorker} from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +15,10 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     provideStore(reducers, {metaReducers}),
     provideStoreDevtools({maxAge: 25, logOnly: !isDevMode()}),
-    provideEffects(VehiclesEffects, MapsEffects, BusesEffects)
+    provideEffects(VehiclesEffects, MapsEffects, BusesEffects),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ]
 };
